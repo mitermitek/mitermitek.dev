7238b1d2339b28df2df3906735557ea2e087178c
Kevin MITERMITE <40229021+mitermitek@users.noreply.github.com>
2022-09-24 23:18:19 +0200

    Add meta and title in head

--- components/layout.js
+++ components/layout.js
@@ -1,9 +1,15 @@
 import Footer from "./footer";
 import Navbar from "./navbar";
+import Head from "next/head";
 
 export default function Layout({ children }) {
   return (
     <>
+      <Head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <title>Kevin Mitermite - French web developer.</title>
+      </Head>
       <Navbar />
       <main className="container mx-auto px-2 sm:px-4 py-2.5 mb-10">
         <div className="max-w-6xl mx-auto">{children}</div>
