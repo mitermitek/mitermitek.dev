21b3495ff2bafc8941cd5e7738e45568fb4bafad
Kevin MITERMITE <40229021+mitermitek@users.noreply.github.com>
2022-09-24 17:37:18 +0200

    Add footer to layout

---
+++ components/footer.js
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer className="bg-white px-2 sm:px-4 py-2.5">
+      <p className="text-center text-sm">Made with ❤️ by Kevin MITERMITE</p>
+    </footer>
+  );
+}
--- components/layout.js
+++ components/layout.js
@@ -1,10 +1,12 @@
+import Footer from "./footer";
 import Navbar from "./navbar";
 
 export default function Layout({ children }) {
   return (
     <>
       <Navbar />
-      <main className="container mx-auto">{children}</main>
+      <main className="container mx-auto mb-10">{children}</main>
+      <Footer />
     </>
   );
 }
