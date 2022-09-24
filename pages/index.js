b580c5a9328d479c8638b0ce98a6489016639172
Kevin MITERMITE <40229021+mitermitek@users.noreply.github.com>
2022-09-24 22:51:41 +0200

    Add my short presentation and my picture

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,43 @@
+import Image from "next/image";
+import kevin from "../public/images/kevin.jpeg";
+
 export default function Home() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <>
+      <div className="grid grid-col-1 md:grid-cols-2 justify-center md:items-center md:space-x-1">
+        <div className="order-2 md:order-1">
+          <h1 className="text-4xl lg:text-6xl font-bold text-blue-600 mb-2 text-center md:text-left">
+            Kevin Mitermite
+          </h1>
+          <p className="text-2xl lg:text-3xl mb-5 text-center md:text-left">
+            Web developer from 🇫🇷 France
+          </p>
+          <p className="font-semibold text-center md:text-left">
+            Currently working at{" "}
+            <a
+              href="https://www.michelin.fr/"
+              target="_blank"
+              rel="noreferrer"
+              className="font-bold text-blue-600 hover:underline"
+            >
+              MICHELIN
+            </a>
+            .
+          </p>
+        </div>
+        <div className="order-1 md:order-2 mb-10 md:mb-0">
+          <div className="w-2/3 mx-auto">
+            <Image
+              alt="Kevin Mitermite"
+              src={kevin}
+              layout="responsive"
+              quality={100}
+              priority={true}
+              className="rounded-full"
+            />
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
