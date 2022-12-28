4eeba09c93bbcf529de5a5c250267ba68d362013
Kevin MITERMITE <40229021+mitermitek@users.noreply.github.com>
2022-12-28 10:33:45 +0100

    Add vercel analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,12 @@
-import "../styles/globals.css";
+import { Analytics } from "@vercel/analytics/react";
 import Layout from "../components/layout";
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
   return (
     <Layout>
       <Component {...pageProps} />
+      <Analytics />
     </Layout>
   );
 }
